136fb1865f99e65732aa37dd34fcb71fa9e6cef4
ninad lanke <ninad lanke>
2016-11-01 17:47:41 -0400

    1.Removed build folder

--- AndroidApplication4.0/Autopilot/SDKDemo/build/intermediates/assets/debug/mapengine_config/shaders/terrain/texture_color_FOG.frag.hlsl
+++
@@ -1,9 +0,0 @@
-void main(	in float4 gl_Position : SV_Position,
-			in float4 color : COLOR,
-			in float eyedist : EYEDIST,
-			out float4 gl_FragColor : SV_Target0)
-{
-	gl_FragColor = color;
-
-	return;
-}
